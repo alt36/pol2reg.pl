5952a052c3e61d72f2a5e9da76c8bcca74527822
Adam Thorn <alt36@cam.ac.uk>
2017-01-05 15:26:28 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # pol2reg.pl
+
+A perl script to convert a Microsoft .pol file (e.g. from Group Policy) into a registry (.reg) file
+
+Usage
+-----
+
+./pol2reg.pl -c HKLM|HKCU input.pol
+
+will read input.pol and dump a corresponding .reg file to standard output. The -c option is mandatory, 
+and must be either HKLM or HKCU, correpsonding to the HKEY_LOCAL_MACHINE and HKEY_CURRENT_USER brances
+of the registry respectively.
+
+Alternatively, the input can be read from stdin:
+
+cat input.pol | ./pol2reg.pl -c HKLM|HKCU
+
+Limitations
+-----------
+The script writes entries for the types REG_SZ, REG_EXPAND_SZ, REG_BINARY, REG_DWORD, 
+REG_DWORD_BIG_ENDIAN, REG_QWORD . Other types are not yet handled.
+
+Notes on the format
+-------------------
+Microsoft document the format of the pol file, but some of it is a little unclear/ambiguous/contradictory, 
+mainly down to the details of the character encodings used (the file is a mix of 16 and 32 bit little-endian
+bytes, which will sometimes be interpreted as encoding a sequence of UTF-16LE characters).
+
+The [Registry Policy File Format][1] says the pol file begins with a signature of 0x67655250, whilst the
+[Registry Policy Message Syntax][2] says the signature is %x50 %x52 %x65 %x67. In fact, the first 4 bytes 
+of the file are 5250 6765; neither document states the byte ordering.
+
+The signature is then followed by a version number (32 bit little-endian), which currently can only be 1. 
+Thus the next 4 bytes will be 0100 0000.
+
+The body then follows after the version number, as a sequence of messages of the form
+
+[key;value;type;size;data]
+
+The [Registry Policy Message Syntax][2] states that the key and the value are UTF-16LE encoded, but doesn't
+specify that the "[", ";" and "]" characters are /also/ UTF-16LE encoded (i.e. the hex sequences to look 
+for are 005b, 003b, and 005d respectively). Similarly, the key and value are null-terminated; this is a 
+UTF-16LE null (0000)
+
+Also, the [Registry Policy Message Syntax][2] gives a specification that is claimed to follow ABNF as 
+specified in RFC4234, but strictly the syntax is /not/ RFC4234 compliant: RFC4234 defines rules such 
+as SP, VCHAR in their ASCII representations, but the rule defining
+
+ ValueCharacter = SP / VCHAR
+
+needs to be read as the UTF-16LE versions of 0x20 or 0x21-7e respectively.
+
+Links
+-----
+Registry Policy File Format:
+
+[1]: https://msdn.microsoft.com/en-us/library/aa374407(v=vs.85).aspx
+
+Registry Policy Message Syntax:
+
+[2]: https://msdn.microsoft.com/en-us/library/cc232696.aspx
---
+++ pol2reg.pl
@@ -0,0 +1,157 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Encode qw(decode encode);
+use Data::Dumper;
+use Getopt::Long;
+
+local $/;
+
+my $context;
+
+GetOptions ("context|=s" => \$context);
+
+sub usage {
+  print "Usage: $0 --context=HKLM|HKCU\n";
+  exit 1;
+}
+
+if (!$context) {
+  usage();
+}
+
+if($context eq 'HKLM') {
+  $context = 'HKEY_LOCAL_MACHINE';
+}elsif($context eq 'HKCU') {
+  $context = 'HKEY_CURRENT_USER';
+} else {
+  usage();
+}
+
+
+my ($sig, $ver, $body)  = unpack('H8 H8 H*', <>);
+$sig == "50526567" or die "bad sig (got $sig)";
+$ver == "01000000" or die "bad version (got $ver)";
+
+my $reading=0;
+
+my @stream = unpack("a2" x (length($body)/2), $body);
+my @instructions = ();
+my $count=0;
+my $element=0;
+my $done=0;
+my $pos=0;
+
+while($pos<scalar(@stream))  {
+  my $byte=$stream[$pos];
+  my $nextbyte=$stream[$pos+1];
+
+  if($byte.$nextbyte eq "5b00") { # [
+    if($reading) {
+      $instructions[$count][$element] .= $byte.$nextbyte;
+    } else {
+      $reading=1;
+    }
+  } elsif($byte.$nextbyte eq "5d00") { # ]
+    $count++;
+    $element=0;
+    $reading=0;
+  } elsif($byte.$nextbyte eq "3b00") { # ;
+    $element++;
+  } else {
+    $instructions[$count][$element] .= $byte.$nextbyte;
+  }
+  if($element == 4) {
+    my $size=hex2int32le($instructions[$count][3]);
+    # suppose ; is bytes 326,327 and size is 4.
+    # then data is 328, 329, 330, 331
+    # and pos should be 332 afterwards
+    if($size > 0) {
+      $instructions[$count][$element] = join('', @stream[$pos+2 .. $pos+$size+1]);
+      $pos+=$size+2;
+    }
+  } else {
+    $pos+=2;
+  }
+}
+
+
+my $currentKey="";
+
+print "Windows Registry Editor Version 5.00\n";
+
+foreach my $inst (@instructions) {
+  my $key = hex2utf16le(stripTerminalNull($inst->[0]));
+  my $value = hex2utf16le(stripTerminalNull($inst->[1]));
+  my $type = hex2int32le($inst->[2]);
+  my $size = hex2int32le($inst->[3]);
+  my $data = $inst->[4];
+
+  if($key ne $currentKey) {
+    $currentKey=$key;
+    print "\n";
+    print "[$context\\$key]\n";
+  }
+
+  
+  if($type == 0) {
+    next;
+  }
+  
+  print "\"$value\"=";
+
+  if($type == 1) { # REG_SZ
+    $data = hex2utf16le($data);
+    print "\"$data\"\n";
+  }
+
+  if($type == 2) { # REG_EXPAND_SZ
+    $data = hex2utf16le($data);
+  }
+
+  if($type == 3) { # REG_BINARY
+   print $data."\n"; 
+  }
+
+  if($type == 4) { # REG_DWORD
+    print "dword:$data\n";
+    $data = hex2int32le($data);
+  }
+
+  if($type == 5) { # REG_DWORD_BIG_ENDIAN
+    $data = hex2int32be($data);
+  }
+
+  if($type == 7) { # REG_MULTI_SZ
+  }
+ 
+  if($type == 11) { # REG_QWORD
+    $data = hex2int64le($data);
+  }
+}
+
+# strip a terminal UTF-16 null (0000)
+sub stripTerminalNull {
+  my $a=shift;
+  $a =~ s/(.*)0000/$1/ ;
+  return $a;
+}
+
+# convert hex string to utf-16le
+# eg given 480065006c006c006f00, return hello
+sub hex2utf16le {
+  return decode("UTF-16LE", pack('H*', shift));
+}
+
+sub hex2int32le {
+  return unpack('L<', pack('H*', shift));
+}
+
+sub hex2int32be {
+  return unpack('L>', pack('H*', shift));
+}
+
+sub hex2int64le {
+  return unpack('Q>', pack('H*', shift));
+}
